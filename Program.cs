7bb5c3f571fa238b46488728777ada9f50096fde
Bruce M. Axtens <bugmagnet@outlook.com>
2020-07-01 21:55:35 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.ClearScript.V8;
+
+namespace HRR
+{
+    class Program
+    {
+        private static V8ScriptEngine v8;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            v8 = new V8ScriptEngine();
+            ConfigureJavaScriptEngine(ref v8);
+            Console.Write("Content-Type: text/html\n\n");
+            if (args.Length > 0)
+            {
+                if (File.Exists(args[0])) 
+                    ParseScript(args[0]);
+            }
+        }
+
+        private static void ConfigureJavaScriptEngine(ref V8ScriptEngine v8)
+        {
+            
+        }
+
+        private static void ParseScript(string v)
+        {
+            var script = File.ReadAllText(v);
+            var pattern = new Regex("<{(.*?)}>", RegexOptions.Singleline | RegexOptions.Compiled);
+            var matches = pattern.Matches(script);
+            foreach (Match match in matches)
+            {
+                if (match.Success)
+                {
+                    var expression = match.Groups[1].Value;
+                    var value = v8.Evaluate(expression);
+                    script = script.Replace(match.Captures[0].Value.ToString(), value.ToString());
+                }
+            }
+            Console.Write(script);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HRR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HRR")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d00d999c-2aa3-44ba-9e6a-ad049495e22d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
